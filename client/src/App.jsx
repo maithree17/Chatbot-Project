de3bdee1945f9b44b4d84faa5c29fbd31c6ac510
maithree17 <maithreemuliya@gmail.com>
2026-03-04 22:27:26 +0530

    changes updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Sidebar from './Components/Sidebar'
+import {Route, Routes} from 'react-router-dom'
+import Chatbox from './Components/Chatbox'
+import Credits from './Pages/Credits'
+import Community from './Pages/Community'
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div>
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Chatbox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App
